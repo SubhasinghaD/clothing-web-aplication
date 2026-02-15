bb2ff0031b9e6b8b7eed57d3ea74611bfd480f46
Subhasingha W. D. S. <dilankasuresh06@gmail.com>
2026-02-15 09:58:54 +0530

    Update mongodb.js

--- backend 02/config/mongodb.js
+++ backend 02/config/mongodb.js
@@ -4,6 +4,16 @@ const connectDB = async () => {
   mongoose.connection.on("connected", () => {
     console.log("DB Connected");
   });
-  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
+
+  const mongoUri = process.env.MONGODB_URL; // use MONGODB_URL from your env
+  if (!mongoUri) {
+    throw new Error("MONGODB_URL is not set");
+  }
+
+  await mongoose.connect(`${mongoUri}/e-commerce`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 };
+
 export default connectDB;
